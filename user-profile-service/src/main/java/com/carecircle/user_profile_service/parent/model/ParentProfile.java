96af0de778a570a86f943cce767612e837b478af
Shivakumar yadav <shivayadavjabbu@gmail.com>
2026-01-23 16:32:51 +0530

    feat(parent): add ParentProfile entity for parent user domain
    Introduces the core parent profile entity used to persist parent-specific profile data. Authentication details are managed externally and referenced via user email.

---
+++ user-profile-service/src/main/java/com/carecircle/user_profile_service/parent/model/ParentProfile.java
@@ -0,0 +1,106 @@
+package com.carecircle.user_profile_service.parent.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.AssertFalse;
+
+/**
+ * Represents a parent user's profile in the  CareCircle Platfom
+ * 
+ * This entity stores domain-specific profile information for a parent. 
+ * Authentication and authorization details are handled by auth-service
+ * and injected via API gateway 
+ */
+
+@Entity
+@Table(
+		name="parent_profiles", 
+		uniqueConstraints = {
+				@UniqueConstraint(columnNames = "user_email")
+		})
+
+public class ParentProfile {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; 
+	
+
+	@Column(name="user_email", nullable = false, updatable = false)
+	private String userEmail; 
+	
+	@Column(name="full_name", nullable=false)
+	private String fullName;
+	
+	@Column(name="phone_number", nullable=false)
+	private String phoneNumber; 
+	
+	@Column(name="address", nullable=false)
+	private String address; 
+	
+	@Column(name="created_at", nullable=false, updatable=false)
+	private LocalDateTime createdAt; 
+	
+	@Column(name="updated_at", nullable=false)
+	private LocalDateTime updatedAt;
+	
+	protected  ParentProfile() {		
+	}
+	
+	public ParentProfile(String userEmail, String fullName, String phoneNumber, String address) {
+        this.userEmail = userEmail;
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+	
+	
+	@PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    // Getters only – entity should not be freely mutable
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+	
+}

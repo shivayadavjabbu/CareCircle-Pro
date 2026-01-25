6d6dbdaa74ac108477d77f9e0cca3aeca5fcc29f
Amreetkumar sinha <amreetanshukumarsinha@gmail.com>
2026-01-26 02:56:10 +0530

    feat(chat): add repository method to fetch room messages
    Added repository query to retrieve chat messages
    for a given room ordered by creation time.
    
    This will be used to expose chat history APIs.

--- communication-service/src/main/java/com/carecircle/communication/repository/chat/ChatMessageRepository.java
+++ communication-service/src/main/java/com/carecircle/communication/repository/chat/ChatMessageRepository.java
@@ -1,5 +1,6 @@
 package com.carecircle.communication.repository.chat;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,6 @@ import com.carecircle.communication.model.chat.ChatMessage;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, UUID> {
 
+	List<ChatMessage> findByRoomIdOrderByCreatedAtAsc(UUID roomId);
+	
 }

ad88871ad61ada56d8d8f0786cba073d3b8131bd
Amreetkumar sinha <amreetanshukumarsinha@gmail.com>
2026-01-26 00:59:11 +0530

    feat(chat): add ChatBlockedException
    Added ChatBlockedException to explicitly represent
    block-list violations during chat interactions.
    
    This exception will be used to replace generic
    runtime exceptions in chat service logic.

---
+++ communication-service/src/main/java/com/carecircle/communication/exception/ChatBlockedException.java
@@ -0,0 +1,8 @@
+package com.carecircle.communication.exception;
+
+public class ChatBlockedException extends RuntimeException {
+
+    public ChatBlockedException(String message) {
+        super(message);
+    }
+}

12cbcf270b0654798548c33b5639e503e8ab2e90
Amreetkumar sinha <amreetanshukumarsinha@gmail.com>
2026-01-26 02:48:42 +0530

    feat(notification): add repository query to fetch user notifications
    Added repository method to retrieve notifications
    for a user ordered by creation time.
    
    This query will be used by notification APIs to
    display in-app notifications to users.

--- communication-service/src/main/java/com/carecircle/communication/repository/notification/NotificationRepository.java
+++ communication-service/src/main/java/com/carecircle/communication/repository/notification/NotificationRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.carecircle.communication.model.notification.Notification;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
-
+	 List<Notification> findByUserIdOrderByCreatedAtDesc(UUID userId);
 }
